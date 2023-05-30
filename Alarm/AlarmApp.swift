a32ce61268e21bb2afe93a04eeff450d6af9d6ae
LukasMod <lukasz.adam.modzelewski@gmail.com>
2023-05-30 10:29:48 +0200

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Łukasz Modzelewski on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
